22d76d3d13b1c17e196c62c4b85ec30bddd8d924
mach1212 <maciej.pruchnik@gmail.com>
2023-08-31 15:19:09 -0400

    [Feat] Add sample dioxus app

--- hello_world_api/src/main.rs
+++ hello_world_api/src/main.rs
@@ -1,3 +1,16 @@
+#![allow(non_snake_case)]
+use dioxus::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    // launch the web app
+    dioxus_web::launch(App);
+}
+
+// create a component that renders a div with the text "Hello, world!"
+fn App(cx: Scope) -> Element {
+    cx.render(rsx! {
+        div {
+            "Hello me"
+        }
+    })
 }
